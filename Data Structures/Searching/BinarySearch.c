8742edb7f319eb45636615290d930cdb51d5581d
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2020-10-09 19:01:01 +0530

    Update BinarySearch.c

--- Data Structures/Searching/BinarySearch.c
+++ Data Structures/Searching/BinarySearch.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int binarySearch(int a[],int low,int high,int key);
-int main()
-{
+
+int main()  {
 	printf("Enter the no. of elements : ");
 	int n;
 		scanf("%d",&n);
 	int a[n];
 	printf("Enter the elements : ");
 	for(int i=0;i<n;i++)
-	{
 		scanf("%d",&a[i]);
-	}
 	printf("Enter the key : ");
 	int key;
 		scanf("%d",&key);
@@ -21,18 +19,16 @@ int main()
 	 	 printf("Key found at index  %d\n",res);
  }
 
-int binarySearch(int a[], int low, int high, int key)
-{
+int binarySearch(int a[], int low, int high, int key) {
     if (low <= high)
     {
       int mid = (low + high) / 2;
-    	if (a[mid] == key)
-           return mid;
-  	    else if (a[mid] > key)
-       		 binarySearch(a, low, mid - 1, key);
-	    	else if (a[mid] < key)
-        	 binarySearch(a, mid + 1, high, key);
-    	}
-  	else
-	  	return -1;
+      if (a[mid] == key)
+          return mid;
+  	  else if (a[mid] > key)
+       	   binarySearch(a, low, mid - 1, key);
+	  else if (a[mid] < key)
+           binarySearch(a, mid + 1, high, key);
+    }
+	 return -1;
 }

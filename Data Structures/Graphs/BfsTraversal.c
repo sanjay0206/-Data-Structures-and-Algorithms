5e5a886ca71158c3647aca7c5b1c00700d46c28b
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2020-10-10 16:57:49 +0530

    Update BfsTraversal.c

--- Data Structures/Graphs/BfsTraversal.c
+++ Data Structures/Graphs/BfsTraversal.c
@@ -1,53 +1,62 @@
-#include<stdio.h>
-#define max 1000
-int adj[max][max],visited[max],i,j,V,E,v,v1,v2;
-int que[max],front=-1,rear=-1;
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stdbool.h>
+#define INF INT_MAX
+#define max 3005
 
+int adj[max][max], V, E, v1, v2, w, source, que[max], front = -1, rear = -1;;
+bool visited[max];
+
+void enqueue(int x){
+	if(front == -1)
+	front++;
+	rear++;
+	que[rear] = x;
+}
+int dequeue(){
+	int x = que[front];
+	front++;
+	return x;
+}
+bool isEmpty() {
+   return (front == -1 || front > rear);
+}
+void addEdge(int v1,int v2) {
+    adj[v1][v2] = 1;
+}
 void create_graph(){
-	 scanf("%d%d", &V,&E);
-    for (i= 1; i <= V; i++)
-	        for (j = 1; j <= V; j++)
-   	         adj[i][j]= 0;
-
- 	for (i = 1; i<=E; i++)
-	{
+     memset(adj,0,sizeof(adj));
+      scanf("%d%d", &V,&E);
+   // int maxEdges = V*(V-1)
+ 	for (int i = 1; i <= E; i++) 
+ 	{
        scanf("%d%d", &v1,&v2);
-       adj[v1][v2] = 1;
+       addEdge(v1,v2);
     }
 }
 void display_graph(){
 	printf("\nAdj Matrix\n");
-	for(i=1;i<=V;i++)
+	for(int i=1;i<=V;i++)
 	{
 		printf("\n");
-		for(j=1;j<=V;j++)
+		for(int j=1;j<=V;j++)
 			printf("%d ",adj[i][j]);
 	}
 }
-void enqueue(int x){
-	if(front==-1)
-	front++;
-	rear++;
-	que[rear]=x;
-}
-int dequeue(){
-	int x=que[front];
-	front++;
-	return x;
-}
 void bfs(int v){
 	int node,w;
-	visited[v]=1;
+	visited[v] = true;;
 	enqueue(v);
-	while(front<=rear)
-    {
-		node=dequeue();
+	while(!isEmpty())
+        {
+		node = dequeue();
 		printf("V%d-",node);
 		for(w=1;w<=V;w++)
 		{
 			if(adj[node][w] && !visited[w])
 			{
-				visited[w]=1;
+				visited[w] = true;
 				enqueue(w);
 			}
 		}
@@ -58,10 +67,10 @@ int main()
 	create_graph();
 	printf("\nGraph Successfully created\n");
 	display_graph();
-	for(v=1;v<=V;v++)
+	for(int v=1;v<=V;v++)
  	   visited[v]=0;
 	printf("\nBFS traversal\n");
-	for(v=1;v<=V;v++)
+	for(int v=1;v<=V;v++)
 	{
 	if(!visited[v])
 		bfs(v);

3bc0274a1a1df2d484c99ba14f8af500588f9f66
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2020-10-10 18:15:26 +0530

    Update DfsUsingStack.c

--- Data Structures/Graphs/DfsUsingStack.c
+++ Data Structures/Graphs/DfsUsingStack.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-#define max 1000
-int adj[max][max],visited[max],v,v1,v2,V,E,i,j;
-int stack[max],top=-1;
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define max 3005
+
+int adj[max][max], V, E, v1, v2, w, source, stack[max],top=-1;
+bool visited[max];
 
 void push(int x){
 	top++;
@@ -12,54 +15,53 @@ int pop(){
 	top--;
 	return del;
 }
-void create(){
-	 scanf("%d%d", &V,&E);
-    for (i=1; i<=V; i++)
-	        for (j=1; j<=V; j++)
-   	         adj[i][j]= 0;
-
-    for (i=1; i<=E; i++)
+void addEdge(int v1,int v2) {
+    adj[v1][v2] = 1;
+}
+void create_graph(){
+    memset(adj, 0, sizeof(adj)); // sets 0  for tall the elements in the visited matrix 
+	scanf("%d%d", &V,&E);
+	 // int maxEdges = V*(V-1)
+    for (int i=1; i<=E; i++)
 	{
- 	scanf("%d%d", &v1,&v2);
-  	adj[v1][v2] = 1;
-   }
+     	scanf("%d%d", &v1,&v2);
+      	addEdge(v1,v2);
+    }
 }
 void display_graph(){
 	printf("\nAdj Matrix\n");
-	for(i=1;i<=V;i++)
+	for(int i=1;i<=V;i++)
 	{
 		printf("\n");
-		for(j=1;j<=V;j++)
+		for(int j=1;j<=V;j++)
 			printf("%d ",adj[i][j]);
 	}
 }
 void dfs(int v){
-	int node;
 	push(v);
-	while(top>=0)
+	while(top >= 0)
 	{
-		node=pop();
+		int node=pop();
 		if(!visited[node])
 		{
 			printf("V%d-",node);
-			visited[node]=1;
+			visited[node] = true;
 		}
 		else
-			continue;
-		for(i=V;i>=1;i--)
+		     continue;	
+		for(int i=V;i>=1;i--)
 			if(adj[node][i] && !visited[i])
-	  		  push(i);
+	  		   push(i);
 	}
 }
 int main()
 {
-	create();
+	create_graph();
 	printf("\nGraph Successfully created\n");
 	display_graph();
-	for(v=1;v<=V;v++)
-		visited[i]=0;
+	memset(visited, false, sizeof(visited));  // sets false for tall the elements in the visited matrix 
 	printf("\nDFS traversal\n");
-	for(v=1;v<=V;v++)
-      if(!visited[v])
+	for(int v=1;v<=V;v++)
+           if(!visited[v])
 		dfs(v);
 }

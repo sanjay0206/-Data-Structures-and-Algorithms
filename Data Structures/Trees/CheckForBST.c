64fd04339853eac49b4d6b4bb77276075090018d
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2021-01-12 20:23:40 +0530

    Add files via upload

---
+++ Data Structures/Trees/CheckForBST.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct node {
+    int data;
+    struct node * left, *right;
+}*root = NULL;
+
+struct node *create(struct node * temp, int val) {
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = val;
+    temp->left = temp->right = NULL;
+return temp;
+}
+
+struct node *insert(struct node *temp,int element) {
+	if(temp == NULL)
+         return create(temp, element);
+	else
+	{
+		if(element < temp->data)
+            temp->left = insert(temp->left, element);
+        else if(element > temp->data)
+            temp->right = insert(temp->right, element);
+     }
+    return temp;
+}
+
+int validate (struct node* root, int  low, long high) {
+        if (root == NULL) 
+            return 1;
+        if ((low != NULL && root->data >= high) || (high != NULL && root->data <= low)) 
+            return 0;
+        return validate (root->left , low, root->data) && validate (root->right, root->data, high);
+}
+
+int  isBST(struct node* temp) { 
+    return validate (temp, INT_MIN, INT_MAX);
+}
+
+int main() {
+    int val, n;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+    isBST(root) == 1 ? printf("It is a BST") : printf("It is not a BST");
+}

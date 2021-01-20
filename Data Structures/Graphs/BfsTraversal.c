657820d1cf4f63ce88a0028f40440eed5a547ac0
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2021-01-20 11:11:54 +0530

    Update BfsTraversal.c

--- Data Structures/Graphs/BfsTraversal.c
+++ Data Structures/Graphs/BfsTraversal.c
@@ -6,34 +6,35 @@
 int adj[max][max], V, E, que[max], front = -1, rear = -1;;
 bool visited[max];
 
-void enqueue(int x){
+void enqueue(int x) {
 	if(front == -1)
 	front++;
 	rear++;
 	que[rear] = x;
 }
-int dequeue(){
+
+int dequeue() {
 	int x = que[front];
 	front++;
 	return x;
 }
-bool isEmpty() {
-   return (front == -1 || front > rear);
-}
+
 void addEdge(int v1,int v2) {
     adj[v1][v2] = adj[v2][v1] = 1;
 }
-void create_graph(){
-      memset(adj,0, sizeof(adj)); // sets 0  for tall the elements in the visited matrix 
+
+void create_graph() {
+      memset(adj,0, sizeof(adj)); // sets 0  for all the elements in the visited matrix 
       scanf("%d%d", &V,&E);
-   // int maxEdges = V*(V-1)
+      // int maxEdges = V*(V-1)
 	int v1, v2;
  	for (int i = 1; i <= E; i++) {
           scanf("%d%d", &v1,&v2);
           addEdge(v1,v2);
     }
 }
-void display_graph(){
+
+void display_graph() {
 	printf("\nAdj Matrix\n");
 	for(int i=1;i<=V;i++)
 	{
@@ -42,10 +43,10 @@ void display_graph(){
 			printf("%d ",adj[i][j]);
 	}
 }
-void bfs(int node)  {
+void bfs(int node) {
 	visited[node] = true;;
 	enqueue(node);
-	while(!isEmpty()) {
+	while(front <= rear) {
 	   node = dequeue();
 	   printf("V%d-",node);
            for(int i=1;i<=V;i++) {
@@ -54,8 +55,9 @@ void bfs(int node)  {
 		    enqueue(i);
 		}
 	   }
-       }
+    }
 }
+
 int main() {
 	create_graph();
 	printf("\nGraph Successfully created\n");

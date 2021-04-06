d32ae4df7203798bfee28a68b551fa0c6f102e42
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2021-04-06 18:10:50 +0530

    Update MaximumHourglasses.c

--- Data Structures/Arrays/MaximumHourglasses.c
+++ Data Structures/Arrays/MaximumHourglasses.c
@@ -1,4 +1,4 @@
-/*Considering that one of the problem constrians is -9 <= arr[i][j] <= 9
+/*Considering that one of the problem constriants is -9 <= arr[i][j] <= 9
 
 You can consider that the lowest Hourglass value will be the one where all the 7 values are -9.
 
@@ -6,38 +6,33 @@ Considering this, you only need to initialize max_value to -63 should be enough.
 
 //possible hourglasses = (row-2)*(column-2)
 #include<stdio.h>
-int MaxhourglassSum=-63;
-const int m=6,n=6;
-int hourglass(int a[m][n],int i,int j)
-{
-	int row,col;
+int MaxhourglassSum = -63;
+int hourglass(int a[6][6],int i,int j) {
     int sum=0;
-            for(row=i;row<i+3;row++){
-                for(col=j;col<j+3;col++){
-                    if(! ((row==i+1 && col==j) || (row==i+1 && col==j+2)) )
-                        sum=sum+a[row][col];
-          			  else
-	     				continue;
-                }
+    for(int row=i;row<i+3;row++){
+        for(int col=j;col<j+3;col++){
+            if(! ((row == i+1 && col == j) || (row == i+1 && col == j+2)) )
+                sum = sum + a[row][col];
+            else
+	     	continue;
         }
-        if(sum>MaxhourglassSum)
-            MaxhourglassSum =sum;
+    }
+        if(sum > MaxhourglassSum)
+            MaxhourglassSum = sum;
+
+    return sum;
 }
-int main()
-{
-	int i,j;
-        int a[m][n];
-        printf("Enter the (6x6) Martix : \n\n");
-        for(i=0;i<m;i++){
-        	for(j=0;j<n;j++){
-            	scanf("%d",&a[i][j]);
-			}
-		}
-   	 for(int i=0;i<m-2;i++){
-        for(int j=0;j<n-2;j++){
-			hourglass(a,i,j);
-			}
-		}
-		printf("Maximum sum of an hourglass = %d",MaxhourglassSum);
-	return 0;
+
+int main() {
+    const int m = 6, n = 6;
+    int a[m][n];
+    for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+		
+    for(int i=0;i<m-2;i++)
+        for(int j=0;j<n-2;j++)
+	     hourglass(a,i,j);
+	
+    printf("%d",MaxhourglassSum);
 }
